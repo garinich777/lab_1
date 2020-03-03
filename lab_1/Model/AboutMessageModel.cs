d771c62399e8f24b5eb89a90108641ad633d54a5
Admin <777programmer777@gmil.com>
2020-03-03 15:09:05 +0300

    Добавлено рамку о программе
    Добавлена рамака о программе. Информация о программе запрашивается AboutMessageVM-ом у AboutMessageModel

---
+++ lab_1/Model/AboutMessageModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1.Model
+{
+    static class AboutMessageModel
+    {
+        public static string GetAboutMessage() =>
+            "Lab_1\n" +
+            "Лабораторная работа №1\n" +
+            "Использование массивов\n" +
+            "Написать программу для разделения\n" +
+            "массива на два размером N/2\n" +
+            "первый с четными индексами, второй с нечетными\n" +
+            "Студент группы 484\n" +
+            "Осипов Игорь Вадимович\n" +
+            "2020 год";
+    }
+}
---
+++ lab_1/ViewModel/AboutMessageVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using lab_1.Model;
+
+namespace lab_1.ViewModel
+{
+    class AboutMessageVM
+    {
+        public string Message { get { return AboutMessageModel.GetAboutMessage(); } }
+    }
+}
