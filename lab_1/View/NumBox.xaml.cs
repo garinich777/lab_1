9b1a45816e55e1355e36498e5ed7a4699f6f7d0d
Admin <777programmer777@gmil.com>
2020-03-09 13:20:42 +0300

    Добавлен новый функционал для NumBox
    добавлена возможность ограничения макс. и мин. значения, а также добавлено свойство Value, содержащее значение введенное в NumBox

--- lab_1/View/NumBox.xaml.cs
+++ lab_1/View/NumBox.xaml.cs
@@ -18,9 +18,40 @@ namespace lab_1.View
 {
     public partial class NumBox : UserControl
     {
+        private int _value;
+        private int _max_value;
+        private int _min_value;
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            protected set { SetValue(ValueProperty, value); }
+        }
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register("Value", typeof(int), typeof(NumBox), new PropertyMetadata(0));
+
+        public int MaxValue
+        {
+            get { return (int)GetValue(MaxValueProperty); }
+            set { SetValue(MaxValueProperty, value); }
+        }
+        public static readonly DependencyProperty MaxValueProperty =
+            DependencyProperty.Register("MaxValue", typeof(int), typeof(NumBox), new PropertyMetadata(10000));
+
+        public int MinValue
+        {
+            get { return (int)GetValue(MinValueProperty); }
+            set { SetValue(MinValueProperty, value); }
+        }
+        public static readonly DependencyProperty MinValueProperty =
+            DependencyProperty.Register("MinValue", typeof(int), typeof(NumBox), new PropertyMetadata(-10000));
+
         public NumBox()
         {
             InitializeComponent();
+            _min_value = MinValue;
+            _max_value = MaxValue;
+            _value = Value;
         }
 
         private void RemoveSymbol()
@@ -35,14 +66,19 @@ namespace lab_1.View
             {
                 Regex _num_regex = new Regex("[^0-9-]");
                 Regex _minus_regex = new Regex("[^0-9]");
+                Regex _minus_zero = new Regex("-0");
+                Regex _start_zero = new Regex("^0.+");
 
                 if (_num_regex.IsMatch(tbNumBox.Text)) RemoveSymbol();                
                 
                 else if (_minus_regex.IsMatch(tbNumBox.Text.Remove(0, 1))) RemoveSymbol();
 
-                int value = 0;
-                int.TryParse(tbNumBox.Text, out value);
-                if (Math.Abs(value) > 10000) RemoveSymbol();
+                else if (_minus_zero.IsMatch(tbNumBox.Text)) RemoveSymbol();
+
+                else if (_start_zero.IsMatch(tbNumBox.Text)) RemoveSymbol();
+
+                int.TryParse(tbNumBox.Text, out _value);
+                if (_value < _min_value || _value > _max_value) RemoveSymbol();
             }
         }
     }
