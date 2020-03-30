2553cbf5459bc3a2fe4e02367b54a7bcf3628dcb
Игорь Осипов <igoryn12@outlook.com>
2020-03-30 20:15:29 +0300

    Настройки программы сохраняются в параметрах, а не в ini файлах

--- lab_1/Model/ParamModel.cs
+++ lab_1/Model/ParamModel.cs
@@ -11,26 +11,17 @@ namespace lab_1.Model
 {
     class ParamModel
     {
-        static private IniData data = new IniData();
-        static private string param_file_name = "Configuration.ini";
-
-        static public bool ShowInfo 
-        { 
+        static public bool ShowInfo
+        {
             get
             {
-                if (File.Exists(param_file_name))
-                {
-                    data = new FileIniDataParser().ReadFile(param_file_name);
-                    string show_info_text = data["UI"]["screen_info"];
-                    return bool.Parse(show_info_text);
-                }
-                return true;
+                return (bool)Properties.Settings.Default["show_info"];
             }
             set
             {
-                data["UI"]["screen_info"] = value.ToString();
-                new FileIniDataParser().WriteFile(param_file_name, data);
-            } 
+                Properties.Settings.Default["show_info"] = value;
+                Properties.Settings.Default.Save();
+            }
         }
     }
 }
