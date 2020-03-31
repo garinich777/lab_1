aa027c79cf284171945b88284fcd2832e52f5621
Игорь Осипов <igoryn12@outlook.com>
2020-03-31 18:48:43 +0300

    Заменен символ "\n" на Environment.NewLine в AboutMessageModel

--- lab_1/Model/AboutMessageModel.cs
+++ lab_1/Model/AboutMessageModel.cs
@@ -1,22 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace lab_1.Model
 {
     static class AboutMessageModel
     {
         public static string GetAboutMessage() =>
-            "Lab_1\n" +
-            "Лабораторная работа №1\n" +
-            "Использование массивов\n" +
-            "Написать программу для разделения\n" +
-            "массива на два, размером N/2\n" +
-            "первый с четными индексами,\n второй с нечетными\n" +
-            "Студент группы 484\n" +
-            "Осипов Игорь Вадимович\n" +
+            "Lab_1" + Environment.NewLine +
+            "Лабораторная работа №1" + Environment.NewLine +
+            "Использование массивов" + Environment.NewLine +
+            "Написать программу для разделения" + Environment.NewLine +
+            "массива на два, размером N/2" + Environment.NewLine +
+            "первый с четными индексами,"+ Environment.NewLine + 
+            "второй с нечетными" + Environment.NewLine +
+            "Студент группы 484" + Environment.NewLine +
+            "Осипов Игорь Вадимович" + Environment.NewLine +
             "2020 год";
     }
 }
